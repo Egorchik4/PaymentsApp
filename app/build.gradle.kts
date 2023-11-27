ae494b3cebb5ab58798b9c1303e52fb1d1f6a7cc
Egorchik4 <EgorDanyluk72@mail.ru>
2023-11-27 22:09:21 +0700

    configure .gradle files/manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
+	id("kotlin-kapt")
+	id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -30,6 +32,14 @@ android {
 	kotlinOptions {
 		jvmTarget = "1.8"
 	}
+	tasks.withType<org.jetbrains.kotlin.gradle.tasks.KaptGenerateStubs> {
+		kotlinOptions {
+			jvmTarget = "1.8"
+		}
+	}
+	buildFeatures {
+		viewBinding = true
+	}
 }
 
 dependencies {
@@ -46,4 +56,16 @@ dependencies {
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+	//activityViewModels()
+	implementation("androidx.fragment:fragment-ktx:1.6.2")
+	//navigationComponent
+	implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+	implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+	//retrofit2
+	implementation("com.squareup.retrofit2:retrofit:2.9.0")
+	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+	implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+	//Hilt
+	implementation("com.google.dagger:hilt-android:2.46.1")
+	kapt("com.google.dagger:hilt-compiler:2.46.1")
 }
